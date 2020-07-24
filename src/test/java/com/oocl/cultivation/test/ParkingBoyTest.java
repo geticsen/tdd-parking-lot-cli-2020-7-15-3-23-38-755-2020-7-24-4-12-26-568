ec99424242cbe9a357d556e83de994c56fa5592f
ZHOUGR2 <286348794@qq.com>
2020-07-24 16:01:19 +0800

    TDD [Green] test:add should_return_right_car_when_parking_boy_parking_cars_after_fetch_car_given_correct_parking_car_ticket test case.

--- src/test/java/com/oocl/cultivation/test/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkingBoyTest.java
@@ -12,29 +12,52 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ParkingBoyTest {
     @Test
-    void should_return_ticket_when_parking_boy_park_car_given_car(){
+    void should_return_ticket_when_parking_boy_park_car_given_car() {
         //given
         ParkingLot parkingLot = new ParkingLot();
         ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
         Car car = new Car();
 
         //when
-        ParkingCarTicket parkingCarTicket= parkingBoy.park(car);
+        ParkingCarTicket parkingCarTicket = parkingBoy.park(car);
 
         //then
         assertNotNull(parkingCarTicket);
     }
+
     @Test
-    void should_return_car_when_fetch_car_given_parking_car_ticket(){
+    void should_return_car_when_fetch_car_given_parking_car_ticket() {
         //given
         ParkingLot parkingLot = new ParkingLot();
         ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
         Car car = new Car();
         Car fetchCar;
+
         //when
-        ParkingCarTicket parkingCarTicket= parkingBoy.park(car);
+        ParkingCarTicket parkingCarTicket = parkingBoy.park(car);
         fetchCar = parkingBoy.fetchCarByParkingTicket(parkingCarTicket);
+
         //then
-        Assertions.assertEquals(car,fetchCar);
+        Assertions.assertEquals(car, fetchCar);
+    }
+
+    @Test
+    void should_return_right_car_when_parking_boy_parking_cars_after_fetch_car_given_correct_parking_car_ticket() {
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Car car3 = new Car();
+        Car fetchCar;
+
+        //when
+        ParkingCarTicket parkingCarTicket1 = parkingBoy.park(car1);
+        ParkingCarTicket parkingCarTicket2 = parkingBoy.park(car2);
+        ParkingCarTicket parkingCarTicket3 = parkingBoy.park(car3);
+        fetchCar = parkingBoy.fetchCarByParkingTicket(parkingCarTicket2);
+
+        //then
+        Assertions.assertEquals(car2, fetchCar);
     }
 }
