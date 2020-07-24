592c2170b861f58089e9f281d452fc3639908839
ZHOUGR2 <286348794@qq.com>
2020-07-24 19:27:18 +0800

    TDD [Green] test:add  should_return_wrong_message_when_fetch_car_given_wrong_ticket test case.

--- src/test/java/com/oocl/cultivation/test/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkingBoyTest.java
@@ -60,15 +60,16 @@ public class ParkingBoyTest {
         //then
         Assertions.assertEquals(car2, fetchCar);
     }
+
     @Test
-    void should_return_null_when_fetch_car_given_wrong_or_not_take_parking_car_ticket(){
+    void should_return_null_when_fetch_car_given_wrong_or_not_take_parking_car_ticket() {
         //given
         ParkingLot parkingLot = new ParkingLot();
         ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
         Car car1 = new Car();
         Car car2 = new Car();
         Car car3 = new Car();
-        ParkingCarTicket parkingCarTicket =new ParkingCarTicket();
+        ParkingCarTicket parkingCarTicket = new ParkingCarTicket();
         Car fetchCar1;
         Car fetchCar2;
 
@@ -82,14 +83,15 @@ public class ParkingBoyTest {
         Assertions.assertEquals(null, fetchCar1);
         Assertions.assertEquals(null, fetchCar2);
     }
+
     @Test
-    void should_return_null_when_fetch_car_given_uesed_parking_car_ticket(){
+    void should_return_null_when_fetch_car_given_uesed_parking_car_ticket() {
         //given
         ParkingLot parkingLot = new ParkingLot();
         ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
         Car car = new Car();
         ParkingCarTicket parkingCarTicket;
-        Car fetchCar,fetchCar0;
+        Car fetchCar, fetchCar0;
         //when
         parkingCarTicket = parkingBoy.park(car);
         fetchCar0 = parkingBoy.fetchCarByParkingTicket(parkingCarTicket);
@@ -97,18 +99,41 @@ public class ParkingBoyTest {
         //then
         Assertions.assertEquals(null, fetchCar);
     }
+
     @Test
-    void should_return_null_when_parking_car_given_car_over_10(){
+    void should_return_null_when_parking_car_given_car_over_10() {
         //given
         ParkingLot parkingLot = new ParkingLot();
         ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
         ParkingCarTicket parkingCarTicket = new ParkingCarTicket();
+
         //when
-        for(int i=1;i<=11;i++){
+        for (int i = 1; i <= 11; i++) {
             Car car = new Car();
             parkingCarTicket = parkingBoy.park(car);
         }
+
         //then
         Assertions.assertEquals(null, parkingCarTicket);
     }
+
+    @Test
+    void should_return_wrong_message_when_fetch_car_given_wrong_ticket() {
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        Car car = new Car();
+        ParkingCarTicket parkingCarTicket;
+        Car fetchCar;
+
+
+        //when
+
+        parkingCarTicket = parkingBoy.park(car);
+        fetchCar = parkingBoy.fetchCarByParkingTicket(parkingCarTicket);
+        fetchCar = parkingBoy.fetchCarByParkingTicket(parkingCarTicket);
+        //then
+        Assertions.assertEquals("Unrecognized parking ticket.", fetchCar);
+    }
+
 }
