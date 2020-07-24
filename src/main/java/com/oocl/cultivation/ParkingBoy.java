363dee5b713475a5326411d0350214cc0e8d4be8
ZHOUGR2 <286348794@qq.com>
2020-07-24 15:55:24 +0800

    TDD [Green] feature:implement ParkingBoy fetch function when given parkingCarTicket return a car.

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -1,10 +1,22 @@
 package com.oocl.cultivation;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ParkingBoy {
+    private Map<ParkingCarTicket, Car> parkingCarList = new HashMap<ParkingCarTicket, Car>();
+
     public ParkingBoy(ParkingLot parkingLot) {
     }
 
     public ParkingCarTicket park(Car car) {
-        return new ParkingCarTicket();
+
+        ParkingCarTicket parkingCarTicket = new ParkingCarTicket();
+        this.parkingCarList.put(parkingCarTicket, car);
+        return parkingCarTicket;
+    }
+
+    public Car fetchCarByParkingTicket(ParkingCarTicket parkingCarTicket) {
+        return this.parkingCarList.get(parkingCarTicket);
     }
 }
