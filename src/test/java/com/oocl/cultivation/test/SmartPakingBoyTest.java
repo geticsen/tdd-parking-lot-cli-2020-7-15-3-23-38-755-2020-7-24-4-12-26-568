2ee4d2c14be336522a347770d25cb519d2492eb6
ZHOUGR2 <286348794@qq.com>
2020-07-25 16:33:18 +0800

    TDD [Green] test: add SmartPakingBoyTest file and add should_return_most_suitable_parking_lot_when_smart_parking_boy_parking_car_given_car test case.

---
+++ src/test/java/com/oocl/cultivation/test/SmartPakingBoyTest.java
@@ -0,0 +1,44 @@
+package com.oocl.cultivation.test;
+
+import com.oocl.cultivation.Car;
+import com.oocl.cultivation.ParkingCarTicket;
+import com.oocl.cultivation.ParkingLot;
+import com.oocl.cultivation.SmartPakingBoy;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SmartPakingBoyTest extends ParkingBoyTest{
+    @Test
+    void should_return_most_suitable_parking_lot_when_smart_parking_boy_parking_car_given_car(){
+        //given
+        ParkingLot parkingLot1 =new ParkingLot();
+        ParkingLot parkingLot2 =new ParkingLot();
+        ParkingLot parkingLot3 =new ParkingLot();
+
+        parkingLot1.storeCar(new Car());
+        parkingLot1.storeCar(new Car());
+
+        parkingLot2.storeCar(new Car());
+        parkingLot2.storeCar(new Car());
+
+        parkingLot3.storeCar(new Car());
+
+        List<ParkingLot> parkingLots =new ArrayList<ParkingLot>();
+        parkingLots.add(parkingLot1);
+        parkingLots.add(parkingLot2);
+        parkingLots.add(parkingLot3);
+        Car car = new Car();
+        ParkingCarTicket parkingCarTicket;
+
+        //when
+        SmartPakingBoy smartPakingBoy =new SmartPakingBoy(parkingLots);
+        parkingCarTicket = smartPakingBoy.park(car);
+        ParkingLot parkingLot = smartPakingBoy.findParkingLotByParkingCarTicket(parkingCarTicket);
+
+        //then
+        Assertions.assertEquals(parkingLot3,parkingLot);
+    }
+}
