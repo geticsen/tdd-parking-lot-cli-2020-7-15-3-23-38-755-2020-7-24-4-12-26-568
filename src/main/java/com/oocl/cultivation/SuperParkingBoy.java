067c8d54c6dc9c722a5d53f19a1f83e93bd1079e
ZHOUGR2 <286348794@qq.com>
2020-07-25 17:28:54 +0800

    TDD [Green] feature: Override SmartPakingBoy attemptPark function in SuperParkingBoy .

---
+++ src/main/java/com/oocl/cultivation/SuperParkingBoy.java
@@ -0,0 +1,29 @@
+package com.oocl.cultivation;
+
+import java.util.List;
+
+public class SuperParkingBoy extends SmartPakingBoy{
+    public SuperParkingBoy(ParkingLot parkingLot) {
+        super(parkingLot);
+    }
+
+    public SuperParkingBoy(List<ParkingLot> parkingLot) {
+        super(parkingLot);
+    }
+
+    @Override
+    public String attemptPark(Car car) {
+        int maxAvilableRate = 0;
+        int parkingLotIndex = -1;
+        for (int i = 0; i < this.getParkingLots().size(); i++) {
+            if (!this.getParkingLots().get(i).isParkingLotFull()) {
+                if (this.getParkingLots().get(i).availableRate()>maxAvilableRate){
+                    maxAvilableRate = this.getParkingLots().get(i).availableCapacity();
+                    parkingLotIndex = i;
+                }
+            }
+        }
+        boolean isValid = parkingLotIndex != -1;
+        return isValid ? String.valueOf(parkingLotIndex) : "Not enough position.";
+    }
+}
