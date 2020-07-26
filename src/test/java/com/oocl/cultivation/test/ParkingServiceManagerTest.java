f08de663f8fde714385198706efb637413afbcad
geticsen <286348794@qq.com>
2020-07-26 22:58:22 +0800

    TDD [Green] test:add should_return_car_when_manager_specify_parking_boy_fetch_car_given_parking_car_ticket test case in ParkingServiceManagerTest file.

--- src/test/java/com/oocl/cultivation/test/ParkingServiceManagerTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkingServiceManagerTest.java
@@ -26,4 +26,22 @@ public class ParkingServiceManagerTest {
         //then
         assertNotNull(parkingCarTicket);
     }
+    @Test
+    void should_return_car_when_manager_specify_parking_boy_fetch_car_given_parking_car_ticket(){
+        //given
+        ParkingServiceManager parkingServiceManager = new ParkingServiceManager();
+        ParkingLot parkingLot =new ParkingLot(10,parkingServiceManager);
+        ParkingBoy parkingBoy =new ParkingBoy(parkingLot);
+        Car car = new Car();
+        ParkingCarTicket parkingCarTicket;
+        Car fetchCar;
+        //when
+        parkingServiceManager.addManageParkingBoy(parkingBoy);
+
+        parkingCarTicket = parkingServiceManager.specifyParkingBoy(parkingBoy).park(car);
+        fetchCar = parkingServiceManager.specifyParkingBoy(parkingBoy).fetchCarByParkingTicket(parkingCarTicket);
+        //then
+        Assertions.assertEquals(car,fetchCar);
+    }
+
 }
