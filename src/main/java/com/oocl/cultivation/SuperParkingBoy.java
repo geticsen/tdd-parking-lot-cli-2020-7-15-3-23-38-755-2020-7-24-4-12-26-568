d637cd50f570773a2a44b8be2f076d55b4888990
geticsen <286348794@qq.com>
2020-07-27 20:44:41 +0800

    refactor [Green] refactor: refactor getAvailableParkingLot with java8 stream.

--- src/main/java/com/oocl/cultivation/SuperParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SuperParkingBoy.java
@@ -10,20 +10,17 @@ public class SuperParkingBoy extends SmartPakingBoy{
     public SuperParkingBoy(List<ParkingLot> parkingLot) {
         super(parkingLot);
     }
-
-
-    public String getAvailableParkingLot(Car car) {
-        int maxAvilableRate = 0;
-        int parkingLotIndex = -1;
-        for (int i = 0; i < this.getParkingLots().size(); i++) {
-            if (this.getParkingLots().get(i).isParkingLotFull()) {
-                if (this.getParkingLots().get(i).availableRate()>maxAvilableRate){
-                    maxAvilableRate = this.getParkingLots().get(i).availableCapacity();
-                    parkingLotIndex = i;
-                }
-            }
+    @Override
+    public ParkingLot getAvailableParkingLot() {
+        ParkingLot parkingLot;
+        try {
+            parkingLot = this.getParkingLots().stream().filter(e -> e.isParkingLotFull())
+                    .sorted((firstLot,secendLot)->{
+                        return (int)((secendLot.availableRate() - firstLot.availableRate())*100);
+                    }).findFirst().get();
+        } catch (Exception e) {
+            parkingLot = null;
         }
-        boolean isValid = parkingLotIndex != -1;
-        return isValid ? String.valueOf(parkingLotIndex) : "Not enough position.";
+        return parkingLot;
     }
 }
