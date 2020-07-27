bbf7882794982d2188b3bb66dc6139e04dc7fb4b
ZHOUGR2 <286348794@qq.com>
2020-07-27 17:05:10 +0800

    Refactor [Green] rafactor:  delete ParkingCarTicket class isUsed attribute and it's set, get function

--- src/main/java/com/oocl/cultivation/ParkingCarTicket.java
+++ src/main/java/com/oocl/cultivation/ParkingCarTicket.java
@@ -1,16 +1,8 @@
 package com.oocl.cultivation;
 
 public class ParkingCarTicket {
-    private boolean isUsed = false;
+
     private String ticketMessage = "";
-
-    public boolean isUsed() {
-        return isUsed;
-    }
-
-    public void setUsed(boolean used) {
-        isUsed = used;
-    }
 
     public String getTicketMessage() {
         return ticketMessage;
